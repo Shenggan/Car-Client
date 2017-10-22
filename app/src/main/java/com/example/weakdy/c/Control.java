14ab25e9bfa86001f7bcddc802fcb6d2007e464c
Hsveh <xfysjtu@gmail.com>
2017-10-22 12:44:59 +0800

    New UI and Control Activity And unfinished Socket

---
+++ app/src/main/java/com/example/weakdy/c/Control.java
@@ -0,0 +1,92 @@
+package com.example.weakdy.c;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Control extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_control);
+        /*
+        findViewById(R.id.pink_icon).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Control.this, "Clicked pink Floating Action Button", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        FloatingActionButton button = (FloatingActionButton) findViewById(R.id.setter);
+        button.setSize(FloatingActionButton.SIZE_MINI);
+        button.setColorNormalResId(R.color.pink);
+        button.setColorPressedResId(R.color.pink_pressed);
+        button.setIcon(R.drawable.ic_fab_star);
+        button.setStrokeVisible(false);
+
+        final View actionB = findViewById(R.id.action_b);
+
+        FloatingActionButton actionC = new FloatingActionButton(getBaseContext());
+        actionC.setTitle("Hide/Show Action above");
+        actionC.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                actionB.setVisibility(actionB.getVisibility() == View.GONE ? View.VISIBLE : View.GONE);
+            }
+        });
+
+        final FloatingActionsMenu menuMultipleActions = (FloatingActionsMenu) findViewById(R.id.multiple_actions);
+        menuMultipleActions.addButton(actionC);
+
+        final FloatingActionButton removeAction = (FloatingActionButton) findViewById(R.id.button_remove);
+        removeAction.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((FloatingActionsMenu) findViewById(R.id.multiple_actions_down)).removeButton(removeAction);
+            }
+        });
+
+        ShapeDrawable drawable = new ShapeDrawable(new OvalShape());
+        drawable.getPaint().setColor(getResources().getColor(R.color.white));
+        ((FloatingActionButton) findViewById(R.id.setter_drawable)).setIconDrawable(drawable);
+
+        final FloatingActionButton actionA = (FloatingActionButton) findViewById(R.id.action_a);
+        actionA.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                actionA.setTitle("Action A clicked");
+            }
+        });
+
+        // Test that FAMs containing FABs with visibility GONE do not cause crashes
+        findViewById(R.id.button_gone).setVisibility(View.GONE);
+
+        final FloatingActionButton actionEnable = (FloatingActionButton) findViewById(R.id.action_enable);
+        actionEnable.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                menuMultipleActions.setEnabled(!menuMultipleActions.isEnabled());
+            }
+        });*/
+
+        //FloatingActionsMenu rightLabels = (FloatingActionsMenu) findViewById(R.id.right_labels);
+        /*FloatingActionButton addedOnce = new FloatingActionButton(this);
+        addedOnce.setTitle("Added once");
+        rightLabels.addButton(addedOnce);
+
+        FloatingActionButton addedTwice = new FloatingActionButton(this);
+        addedTwice.setTitle("Added twice");
+        //addedTwice.setSize(10);
+        rightLabels.addButton(addedTwice);
+        rightLabels.removeButton(addedTwice);
+        rightLabels.addButton(addedTwice);*/
+    }
+}
--- app/src/main/java/com/example/weakdy/c/MainActivity.java
+++ app/src/main/java/com/example/weakdy/c/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.weakdy.c;
 
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.net.wifi.WifiManager;
 import android.net.wifi.WifiInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -18,12 +21,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import me.drakeet.materialdialog.MaterialDialog;
 
 public class MainActivity extends AppCompatActivity {
     public BluetoothSocket mmSocket;
     public static OutputStream mmOutputStream;
     public static InputStream mmInputStream;
     //public SocketServer server;
+
 
     public static BufferedInputStream inputStream = null;
     public static BufferedOutputStream outputStream = null;
@@ -35,16 +40,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_main);
         socket = new Socket();
+
+
         //Set IP Address
-        TextView IP_content = (TextView) findViewById(R.id.IP_content);
-        IP_content.setText("Your IP Address is:" + getIpAddress());
-        TextView wifi_info = (TextView) findViewById(R.id.wifi_info);
-        if(get_wifi_info().equals("<unknown ssid>")){
-            wifi_info.setText("No WIFI Connection");
-        }
-        else wifi_info.setText("Wifi:" + get_wifi_info());
+        TextView IP = (TextView) findViewById(R.id.IP);
+        final TextView IP_address = (TextView) findViewById(R.id.IP_address);
+        final TextView wifi = (TextView) findViewById(R.id.wifi);
+        IP.setTextSize(TypedValue.COMPLEX_UNIT_SP, 35);
+        IP_address.setTextSize(TypedValue.COMPLEX_UNIT_SP, 35);
+        wifi.setTextSize(TypedValue.COMPLEX_UNIT_SP, 35);
+
+        refresh(IP_address,wifi);
+
+
+        final MaterialDialog mMaterialDialog = new MaterialDialog(this)
+                .setTitle("注意事项")
+                .setMessage("请确保WIFI处于连接状态!\n请确保所连接的设备在同一局域网下！");
+        mMaterialDialog.setPositiveButton("OK", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMaterialDialog.dismiss();
+            }
+        }).setNegativeButton("CANCEL", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMaterialDialog.dismiss();
+            }
+        });
+
+
         Button login = (Button) findViewById(R.id.login);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,8 +83,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    public void refresh(TextView IP_address,TextView wifi){
+        IP_address.setText(getIpAddress());
+        if(get_wifi_info().equals("<unknown ssid>")){
+            wifi.setText("No WIFI Connection");
+        }
+        else wifi.setText("WIFI:"+get_wifi_info());
+    }
+
     private String getIpAddress(){
-        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+        WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
         int ipAddress = wifiManager.getConnectionInfo().getIpAddress();
         String ipAddressFormatted = String.format("%d.%d.%d.%d", (ipAddress & 0xff), (ipAddress >> 8 & 0xff),
                 (ipAddress >> 16 & 0xff), (ipAddress >> 24 & 0xff));
@@ -62,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private String get_wifi_info(){
-        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+        WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
         WifiInfo wi = wifiManager.getConnectionInfo();
         return wi.getSSID();
     }
